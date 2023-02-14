ebf56bc66cf441f44d2b88fb65d351a729038055
seungjun0423 <sibonum@gmail.com>
2023-02-15 02:10:49 +0900

    디렉토리 구조 1차

--- front/src/App.js
+++ front/src/App.js
@@ -17,9 +17,8 @@ const Wrap = styled.div`
   margin-left:auto;
 `
 
-const Nav = styled.div`
+const Div = styled.div`
   box-sizing:border-box;
-  height:150px;
   background-color:orange;
   position: -webkit-sticky;
   position: sticky;
@@ -27,28 +26,38 @@ const Nav = styled.div`
   border: solid 0.1px orange ;
   text-align:center;
   font-weight:bold;
-  font-size:60px;
+  font-size:100px;
   padding-top:40px;
   font-family: 'Nanum Gothic', sans-serif;
   color:#ffffff;
-  z-index:1;
+  @media(max-width:520px){
+    font-size:40px;
+  }
 `
 
 const Button = styled.button`
+  display:block;
   width:50px;
   height:40px;
   border-radius:10px;
   background:skyblue;
+  margin-top:10px;
 `
 
 
 const Footer = styled.div`
   width:100%;
-  height:100%;
+  height:100px;
   bottom:0px;
   border: solid 1px #cbcbcb;
   font-family: 'Nanum Gothic', sans-serif;
   background-color:#cbcbcb;
+  font-size:25px;
+  padding-top:50px;
+
+  @media(max-width:520px){
+    font-size:16px;
+  }
 `
 
 const App = () => {
@@ -62,34 +71,34 @@ const App = () => {
 
   return (
     <>
-      <Nav>
+      <Div>
         음성GPT
-      </Nav>
-      <Main>
-        <Wrap>
-          <p style={{ textAlign:'center' }}>
-            {listening ? 'on' : 'off'}
-          </p>
-          <Button 
-            onClick={SpeechRecognition.startListening}>
-            Start
-          </Button>
-          <Button onClick={SpeechRecognition.stopListening}>
-            Stop
-          </Button>
-          <Button onClick={resetTranscript}>
-            Reset
-          </Button>
-          <p>
-            {transcript}
-          </p>
-        </Wrap>
-      </Main>
-
-      <Footer>
-        안녕하세요 저는 이승준입니다! <br/>
-        저는 웹 개발자입니다.
-      </Footer>
+        <Main>
+          <Wrap>
+            <div style={{ marginTop:'100px'}}>
+              {listening ? 'on' : 'off'}
+            </div>
+            <Button 
+              onClick={SpeechRecognition.startListening}>
+              Start
+            </Button>
+            <Button onClick={SpeechRecognition.stopListening}>
+              Stop
+            </Button>
+            <Button onClick={resetTranscript}>
+              Reset
+            </Button>
+            <p>
+              {transcript}
+            </p>
+          </Wrap>
+        </Main>
+    
+        <Footer>
+          안녕하세요 저는 어플을 만든 웹 개발자 이승준입니다! <br/>
+          저에 대해 궁금하시다면 여기를 클릭해주세요!
+        </Footer>
+      </Div>
     </>
 
   );
