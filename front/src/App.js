97314cc89a3bfb24bea6b702908f2aaa00cf9405
seungjun0423 <sibonum@gmail.com>
2023-02-14 19:58:36 +0900

    styled-components Test

--- front/src/App.js
+++ front/src/App.js
@@ -1,16 +1,34 @@
 import { useState, useEffect } from 'react';
 import styled from "styled-components";
 
+const Html = styled.div`
+  width:100%;
+  height:100%;
+`
+
 const Nav = styled.div`
-  height:100px;
-  background-color:black;
+  box-sizing:border-box;
+  height:150px;
+  background-color:orange;
+  position: -webkit-sticky;
+  position: sticky;
+  top:0;
+  border: solid 0.1px orange ;
+  text-align:center;
+  font-weight:bold;
+  font-size:50px;
+  padding-top:30px;
+  font:pr
 `
 
 const App = () => {
   return (
     <>
-    <Nav>
-    </Nav>
+      <Html>
+        <Nav>
+          음성GPT
+        </Nav>
+      </Html>
     </>
 
   );
